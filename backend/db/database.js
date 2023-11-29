79a3914d88631968c512488d946f2da6e6406d04
Upegui2005 <emmaupegui2005@gmail.com>
2023-11-29 02:25:26 -0500

    3.0.0

--- backend/db/database.js
+++ backend/db/database.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusteradsoficha2557466.hbpe9cs.mongodb.net/?retryWrites=true&w=majority`
 
 function connect() {
-    console.log(uri)
     mongoose.connect(uri)
     .then(() => {
         console.log('Conectado')
